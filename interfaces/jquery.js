801c10c82674761b3a54b918b346043e45631b4d
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-12-23 16:46:57 +1300

    A better way to provide $

--- interfaces/jquery.js
+++ interfaces/jquery.js
@@ -3,16 +3,25 @@ declare class _jQueryCallbacks_ {
   attributeCallback(index: number, attr: string): string | number;
 }
 
+type OBJECT_OR_ARRAY = Array | Object;
+
 declare class _jQuery_ {
-  scrollTop(): number;
-  width(): number;
+  constructor(element: Element | string): _jQuery_;
+
   attr(attributeName: string): string;
+  attr(attributeName: string, attributeCallback: _jQueryCallbacks_.attributeCallback): _jQuery_;
   attr(attributeName: string, value: string | number): _jQuery_;
   attr(attributes: Object): _jQuery_;
-  attr(attributeName: string, attributeCallback: _jQueryCallbacks_.attributeCallback): _jQuery_;
+  scrollTop(): number;
+  width(): number;
+
+  static extend(target: OBJECT_OR_ARRAY, ...w: OBJECT_OR_ARRAY): OBJECT_OR_ARRAY;
+  static extend(deep: boolean, target: OBJECT_OR_ARRAY, ...w: OBJECT_OR_ARRAY): OBJECT_OR_ARRAY;
 }
 
-var $ = function(Element) {
-  return new _jQuery_;
+declare module "jQuery" {
+  declare function $(element: Element | string): _jQuery_;
 }
+
+var $ = require('$').$;
 
