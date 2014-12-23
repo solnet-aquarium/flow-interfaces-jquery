3b423ec0b42bbfcb44e67a9404df038af97abb11
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-12-23 14:07:46 +1300

    Added attr, sorted methods alphabetically

--- interfaces/jquery.js
+++ interfaces/jquery.js
@@ -1,6 +1,15 @@
+/* jshint ignore: start */
+declare class _jQueryCallbacks_ {
+  attributeCallback(index: number, attr: string): string | number;
+}
+
 declare class _jQuery_ {
   scrollTop(): number;
   width(): number;
+  attr(attributeName: string): string;
+  attr(attributeName: string, value: string | number): _jQuery_;
+  attr(attributes: Object): _jQuery_;
+  attr(attributeName: string, attributeCallback: jQueryCallbacks.attributeCallback): _jQuery_;
 }
 
 var $ = function(Element) {
